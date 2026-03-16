e6f7c28c11be0742c55ceffd84948b7a3a9e11f1
Carlos Wagner Gonçalves Bittencourt <263085854+WagnerBittencourt-est@users.noreply.github.com>
2026-03-16 19:12:57 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,40 @@
+#include <stdio.h>
 
+int main(){
+
+  float temperatura, umidade;
+  unsigned int estoque, estoqueMinimo=1000;
+  printf("Entre com a temperatura: \n");
+  scanf("%f, &temperatura");
+  printf("Entre com a umidade: \n");
+  scanf("%f, &umidade");
+  printf("Entre com a estoque: \n");
+  sanf("%f, &estoque);
+
+  if(temperatura > 30){
+    printf("Temperatura esta alta \n");
+  } 
+  else
+  {
+    printf("Temperatura esta alta \n");
+  }
+
+
+  if(umidade > 50){
+    printf("umidade elevada \n");
+  } 
+  else
+  {
+    printf("Temperatura dentro dos parametros \n");
+  }
+
+  if(estoque < estoqueMinimo){
+    printf("Estoque abaixo do mínimo \n");
+  }
+  else
+  {
+    printf("Estoque Normal. \n);
+  }
+
+
+}
